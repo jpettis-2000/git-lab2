ccd6658b43a88d13f51a4f64a1f328124207f234
Joseph Pettis <jp554118@ohio.edu>
2020-09-04 15:05:08 -0400

    Updated the repository

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Joseph Pettis
+ *        Date: 09/04/2020
+ * Description: A simple program for the github lab;
+ * prints the statement "Hello Git!"
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!" << endl;
+    return 0;
+}// main
